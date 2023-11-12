6325d6301726102a7346f0606c5c33ab87575166
JKgmr <jkgamez101@gmail.com>
2023-11-12 13:01:02 -0700

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,171 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       VEX                                                       */
+/*    Created:      Thu Sep 26 2019                                           */
+/*    Description:  Clawbot Competition Template                              */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Controller1          controller                    
+// Drivetrain           drivetrain    1, 10, D        
+// ClawMotor            motor         3               
+// ArmMotor             motor         8               
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include<bits/stdc++.h>
+#include "vex.h"
+
+using namespace vex;
+using namespace std;
+
+// A global instance of competition
+competition Competition;
+
+// define your global instances of motors and other devices here
+
+int clawMotorPosition = 0; // setting default position of claw motor
+int clawDestination = 0; // destination of claw motor
+
+void spinClawMotor(){
+  if (clawMotorPosition == clawDestination) ClawMotor.spin(vex::forward, 0, voltageUnits::volt);;
+
+  if (clawDestination > clawMotorPosition){
+    ClawMotor.spin(vex::forward, 7, voltageUnits::volt);
+    clawMotorPosition++;
+  } else if (clawDestination < clawMotorPosition){
+    ClawMotor.spin(vex::forward, -7, voltageUnits::volt);
+    clawMotorPosition--;
+  }
+}
+
+directionType findDirFromVolt(int volts){
+  if (volts >= 0) return vex::forward;
+  else return vex::reverse;
+}
+
+int valueNotZero(int a, int b){
+  if (a != 0) return a;
+  else return b;
+}
+
+int valueNotZero(double a, double b){
+  if (a != 0.0) return a;
+  else return b;
+}
+
+int signs(double a){
+  if (a >= 0.0) return 1;
+  else return -1;
+}
+
+/*---------------------------------------------------------------------------*/
+/*                          Pre-Autonomous Functions                         */
+/*                                                                           */
+/*  You may want to perform some actions before the competition starts.      */
+/*  Do them in the following function.  You must return from this function   */
+/*  or the autonomous and usercontrol tasks will not be started.  This       */
+/*  function is only called once after the V5 has been powered on and        */
+/*  not every time that the robot is disabled.                               */
+/*---------------------------------------------------------------------------*/
+
+void pre_auton(void) {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  
+  // All activities that occur before the competition starts
+  // Example: clearing encoders, setting servo positions, ...
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              Autonomous Task                              */
+/*                                                                           */
+/*  This task is used to control your robot during the autonomous phase of   */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+void autonomous(void) {
+
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              User Control Task                            */
+/*                                                                           */
+/*  This task is used to control your robot during the user control phase of */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+void usercontrol(void) {
+  // User control code here, inside the loop
+
+  while (1) {
+
+    // wheel movement
+
+    // joystick percentages range from -100 to 100 (left to right/down to up)
+    // Axis 3-4 are left joystick, 3 is up and down while 4 is left to right
+    // Axis 1-2 are right joystick, 2 is up and down while 1 is left to right
+
+    Brain.Screen.clearLine();
+    Brain.Screen.print(clawMotorPosition);
+
+    double turnVolts = valueNotZero(Controller1.Axis1.position(percent) * 0.075 / 2, signs(Controller1.Axis4.position(percent)) * max(0, (int)(abs(Controller1.Axis4.position(percent)) - 75)) * 0.2);
+    double forwardVolts = Controller1.Axis3.position(percent) * 0.075;
+
+    int leftVolts = forwardVolts - turnVolts;
+    int rightVolts = forwardVolts + turnVolts;
+
+    LeftMotor.spin(vex::forward, leftVolts, voltageUnits::volt);
+    RightMotor.spin(vex::forward, rightVolts, voltageUnits::volt);
+
+    // claw and arm movement
+
+    int r1 = Controller1.ButtonR1.pressing();
+    int l1 = -Controller1.ButtonL1.pressing();
+
+    int r2 = Controller1.ButtonR2.pressing();
+    int l2 = -Controller1.ButtonL2.pressing();
+
+    if (clawDestination == 0 && r1){
+      clawDestination = 65;
+    } else if (clawDestination == 65 && l1){
+      clawDestination = 0;
+    }
+
+    spinClawMotor(); // testing github
+
+    ArmMotor.spin(vex::forward, (r2+l2)*3, voltageUnits::volt);
+
+    wait(20, msec); // Sleep the task for a short amount of time to
+                    // prevent wasted resources.
+  }
+}
+
+//
+// Main will set up the competition functions and callbacks.
+//
+int main() {
+  // Set up callbacks for autonomous and driver control periods.
+  Competition.autonomous(autonomous);
+  Competition.drivercontrol(usercontrol);
+
+  // Run the pre-autonomous function.
+  pre_auton();
+
+  // Run the autonomous function.
+  autonomous();
+
+  // Prevent main from exiting with an infinite loop.
+  while (true) {
+    wait(100, msec);
+  }
+}
