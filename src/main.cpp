dc86423a7404a0e32c92aad9c5a9edb9bb0890e6
JKgmr <70977623+JKgmr101@users.noreply.github.com>
2023-11-17 07:50:25 +0900

    finding driver isseue

--- src/main.cpp
+++ src/main.cpp
@@ -185,13 +185,14 @@ void usercontrol(void) {
 int main() {
   // Set up callbacks for autonomous and driver control periods.
   Competition.autonomous(autonomous);
-  Competition.drivercontrol(usercontrol);
 
   // Run the pre-autonomous function.
   pre_auton();
 
   // Run the autonomous function.
   autonomous();
+
+  Competition.drivercontrol(usercontrol);
 
   // Prevent main from exiting with an infinite loop.
   while (true) {
